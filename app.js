9e62036c9504e1de1f54b17c42bc4f9f0e021361
1988db <elozrana@gmail.com>
2022-02-02 00:35:09 +0100

    ready to write JS

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    console.log('DOMContentLoaded');
+    const showPlannerBtn = document.querySelector('.show-planner');
+    console.log(showPlannerBtn)
+    const plannerContainer = document.querySelector('.planner-container');
+    console.log(plannerContainer);
+    
 })
