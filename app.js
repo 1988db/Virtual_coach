d780eb9838f168e1042cf6ad0301ebf4dccf0399
1988db <elozrana@gmail.com>
2022-02-02 22:41:54 +0100

    function addExercise added

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     const showPlannerBtn = document.querySelector('.show-planner');    
     const plannerContainer = document.querySelector('.planner-container');
+    const addExerciseBtn = document.querySelector('.add-exercise-btn');
     const training = [];
     
+    //add new exercise from and push exercise object to the training array
+    addExerciseBtn.addEventListener('click', addExercise);
+    function addExercise() {
+        let exercise = new Exercise(0, true, 0, 0, 0, 0, false, '');
+        training.push(exercise);
+        console.log(exercise);
+    }
+
     //exercise template
-    class exercise {
+    class Exercise {
         constructor(duration, power, downLimit, upLimit, downRPMLimit, upRPMLimit, rest, notes) {
             this.duration = duration;
             this.power = power;
