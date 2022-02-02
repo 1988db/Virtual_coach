cd9d23f5b900a4a2291ddd4de9aee4bbe2d1cea1
1988db <elozrana@gmail.com>
2022-02-02 19:34:05 +0100

    exercise template added

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    const showPlannerBtn = document.querySelector('.show-planner');
-    console.log(showPlannerBtn)
+    const showPlannerBtn = document.querySelector('.show-planner');    
     const plannerContainer = document.querySelector('.planner-container');
-    console.log(plannerContainer);
+    const training = [];
+    
+    //exercise template
+    class exercise {
+        constructor(duration, power, downLimit, upLimit, downRPMLimit, upRPMLimit, rest, notes) {
+            this.duration = duration;
+            this.power = power;
+            this.downLimit = downLimit;
+            this.upLimit = upLimit;
+            this.downRPMLimit = downRPMLimit;
+            this.upRPMLimit = upRPMLimit;
+            this.rest = rest;
+            this.notes = notes;
+        }
+    }
+    
     
 })
