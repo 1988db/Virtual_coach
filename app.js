f4ab5dc1fda4a42012b84524ed44fc97f1dffb8a
1988db <elozrana@gmail.com>
2022-02-03 01:06:56 +0100

    remove exercise function add

--- app.js
+++ app.js
@@ -2,14 +2,38 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const showPlannerBtn = document.querySelector('.show-planner');    
     const plannerContainer = document.querySelector('.planner-container');
     const addExerciseBtn = document.querySelector('.add-exercise-btn');
-    const training = [];
+    const exercisesContainer = document.querySelector('.exercises-container');
+    let currentExerciseId = 0;
+    const training = {};
     
     //add new exercise from and push exercise object to the training array
     addExerciseBtn.addEventListener('click', addExercise);
     function addExercise() {
         let exercise = new Exercise(0, true, 0, 0, 0, 0, false, '');
-        training.push(exercise);
-        console.log(exercise);
+        training['id' + currentExerciseId] = exercise;
+        currentExerciseId++;
+        addExerciseForm(currentExerciseId); //adds exercise from
+    }
+
+    function addExerciseForm(currentExerciseId) {
+        const form = document.createElement('form');
+        form.classList.add('add-exercise');
+        form.dataset.id = currentExerciseId;
+        exercisesContainer.appendChild(form);
+        const button = document.createElement('button');
+        button.innerText = 'X';
+        button.classList.add('remove-excercise-btn');
+        button.dataset.id = currentExerciseId;
+        button.addEventListener('click', removeExercise);
+        form.appendChild(button);
+        const durationInputContainer = document.createElement('div');
+        
+    }
+
+    //remove exercise
+    function removeExercise(e) {
+        e.preventDefault();
+        console.log('close ' + e.target.dataset.id)
     }
 
     //exercise template
