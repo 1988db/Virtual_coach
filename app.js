2124986469c5b33e10f5cab47fc536cec985413e
1988db <elozrana@gmail.com>
2022-02-03 17:39:28 +0100

    addExerciseForm function complete

--- app.js
+++ app.js
@@ -21,13 +21,147 @@ document.addEventListener('DOMContentLoaded', ()=> {
         form.dataset.id = currentExerciseId;
         exercisesContainer.appendChild(form);
         const button = document.createElement('button');
+        //close button
         button.innerText = 'X';
         button.classList.add('remove-excercise-btn');
         button.dataset.id = currentExerciseId;
         button.addEventListener('click', removeExercise);
         form.appendChild(button);
+        //exercise name
+        const exerciseNameContainer = document.createElement('div');
+        exerciseNameContainer.classList.add('exercise-name-input-container');
+        form.appendChild(exerciseNameContainer);
+        const exerciseNameLabel = document.createElement('label');
+        exerciseNameLabel.setAttribute('for', 'exerciseName');
+        exerciseNameLabel.innerText = 'Exercise name';
+        exerciseNameContainer.appendChild(exerciseNameLabel);
+        const exerciseNameInput = document.createElement('input');
+        exerciseNameInput.setAttribute('name', 'notes');        
+        exerciseNameInput.setAttribute('type', 'text');        
+        exerciseNameInput.classList.add('number-input');
+        exerciseNameContainer.appendChild(exerciseNameInput);
+        //duration
         const durationInputContainer = document.createElement('div');
+        durationInputContainer.classList.add('duration-input-container');
+        form.appendChild(durationInputContainer);
+        const durationLabel = document.createElement('label');
+        durationLabel.setAttribute('for', 'duration');
+        durationLabel.innerText = 'Duration';
+        durationInputContainer.appendChild(durationLabel);
+        const durationInput = document.createElement('input');
+        durationInput.setAttribute('name', 'duration');
+        durationInput.setAttribute('type', 'number');
+        durationInput.classList.add('number-input');
+        durationInputContainer.appendChild(durationInput);
         
+        //power/hr limits
+        const fieldset = document.createElement('fieldset');
+        form.appendChild(fieldset);
+        const legend = document.createElement('legend');
+        legend.innerText = 'Limits';
+        fieldset.appendChild(legend);
+        //limits type
+        const limitTypeInputsContainer = document.createElement('div');
+        limitTypeInputsContainer.classList.add('limit-type-inputs');
+        fieldset.appendChild(limitTypeInputsContainer);
+        //power
+        const limitPowerTypeInputContainer = document.createElement('div');
+        limitPowerTypeInputContainer.classList.add('limit-power-type-input-container');        
+        limitTypeInputsContainer.appendChild(limitPowerTypeInputContainer);
+        const powerLabel = document.createElement('label');
+        powerLabel.setAttribute('for', 'power');
+        powerLabel.innerText = 'Power';
+        limitPowerTypeInputContainer.appendChild(powerLabel);
+        const powerInput = document.createElement('input');
+        powerInput.setAttribute('name', 'limit-type');
+        powerInput.setAttribute('id', 'power');
+        powerInput.setAttribute('value', 'power');
+        powerInput.setAttribute('type', 'radio');
+        powerInput.setAttribute('checked', true);
+        powerInput.classList.add('number-input');
+        limitPowerTypeInputContainer.appendChild(powerInput);
+        //hr
+        const limitHrTypeInputContainer = document.createElement('div');
+        limitHrTypeInputContainer.classList.add('limit-hr-type-input-container');
+        limitTypeInputsContainer.appendChild(limitHrTypeInputContainer);
+        const hrLabel = document.createElement('label');
+        hrLabel.setAttribute('for', 'hr');
+        hrLabel.innerText = 'HR';
+        limitHrTypeInputContainer.appendChild(hrLabel);
+        const hrInput = document.createElement('input');
+        hrInput.setAttribute('name', 'limit-type');
+        hrInput.setAttribute('id', 'hr');
+        hrInput.setAttribute('value', 'hr');
+        hrInput.setAttribute('type', 'radio');        
+        hrInput.classList.add('number-input');
+        limitHrTypeInputContainer.appendChild(hrInput);
+        //inputs
+        //lower limit
+        const lowerLimitInputContainer = document.createElement('div');
+        lowerLimitInputContainer.classList.add('lower-limit-input-container');
+        fieldset.appendChild(lowerLimitInputContainer);
+        const lowerLimitLabel = document.createElement('label');
+        lowerLimitLabel.setAttribute('for', 'lower-limit');
+        lowerLimitLabel.innerText = 'Lower limit';
+        lowerLimitInputContainer.appendChild(lowerLimitLabel);
+        const lowerLimitInput = document.createElement('input');
+        lowerLimitInput.setAttribute('name', 'lower-limit');        
+        lowerLimitInput.setAttribute('type', 'number');        
+        lowerLimitInput.classList.add('number-input');
+        lowerLimitInputContainer.appendChild(lowerLimitInput);
+        //upper limit
+        const upperLimitInputContainer = document.createElement('div');
+        upperLimitInputContainer.classList.add('upper-limit-input-container');
+        fieldset.appendChild(upperLimitInputContainer);
+        const upperLimitLabel = document.createElement('label');
+        upperLimitLabel.setAttribute('for', 'upper-limit');
+        upperLimitLabel.innerText = 'Upper limit';
+        upperLimitInputContainer.appendChild(upperLimitLabel);
+        const upperLimitInput = document.createElement('input');
+        upperLimitInput.setAttribute('name', 'upper-limit');        
+        upperLimitInput.setAttribute('type', 'number');        
+        upperLimitInput.classList.add('number-input');
+        upperLimitInputContainer.appendChild(upperLimitInput);
+
+        //cadence lower limit
+        const lowerCadenceLimitInputContainer = document.createElement('div');
+        lowerCadenceLimitInputContainer.classList.add('lower-cadence-limit-input-container');
+        form.appendChild(lowerCadenceLimitInputContainer);
+        const cadenceLowerLimitLabel = document.createElement('label');
+        cadenceLowerLimitLabel.setAttribute('for', 'lower-cadence-limit');
+        cadenceLowerLimitLabel.innerText = 'Lower RPM limit';
+        lowerCadenceLimitInputContainer.appendChild(cadenceLowerLimitLabel);
+        const cadenceLowerLimitInput = document.createElement('input');
+        cadenceLowerLimitInput.setAttribute('name', 'lower-cadence-limit');        
+        cadenceLowerLimitInput.setAttribute('type', 'number');        
+        cadenceLowerLimitInput.classList.add('number-input');
+        lowerCadenceLimitInputContainer.appendChild(cadenceLowerLimitInput);
+        //cadence upper limit
+        const upperCadenceLimitInputContainer = document.createElement('div');
+        upperCadenceLimitInputContainer.classList.add('upper-cadence-limit-input-container');
+        form.appendChild(upperCadenceLimitInputContainer);
+        const cadenceUpperLimitLabel = document.createElement('label');
+        cadenceUpperLimitLabel.setAttribute('for', 'upper-cadence-limit');
+        cadenceUpperLimitLabel.innerText = 'Upper RPM limit';
+        upperCadenceLimitInputContainer.appendChild(cadenceUpperLimitLabel);
+        const cadenceUpperLimitInput = document.createElement('input');
+        cadenceUpperLimitInput.setAttribute('name', 'upper-cadence-limit');        
+        cadenceUpperLimitInput.setAttribute('type', 'number');        
+        cadenceUpperLimitInput.classList.add('number-input');
+        upperCadenceLimitInputContainer.appendChild(cadenceUpperLimitInput);
+        //notes
+        const notesInputContainer = document.createElement('div');
+        notesInputContainer.classList.add('notes-input-container');
+        form.appendChild(notesInputContainer);
+        const notesLabel = document.createElement('label');
+        notesLabel.setAttribute('for', 'notes');
+        notesLabel.innerText = 'Notes';
+        notesInputContainer.appendChild(notesLabel);
+        const notesInput = document.createElement('input');
+        notesInput.setAttribute('name', 'notes');        
+        notesInput.setAttribute('type', 'text');        
+        notesInput.classList.add('number-input');
+        notesInputContainer.appendChild(notesInput);        
     }
 
     //remove exercise
@@ -38,13 +172,13 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     //exercise template
     class Exercise {
-        constructor(duration, power, downLimit, upLimit, downRPMLimit, upRPMLimit, rest, notes) {
+        constructor(duration, power, lowerLimit, upperLimit, lowerRPMLimit, upperRPMLimit, rest, notes) {
             this.duration = duration;
             this.power = power;
-            this.downLimit = downLimit;
-            this.upLimit = upLimit;
-            this.downRPMLimit = downRPMLimit;
-            this.upRPMLimit = upRPMLimit;
+            this.lowerLimit = lowerLimit;
+            this.upperLimit = upperLimit;
+            this.lowerRPMLimit = lowerRPMLimit;
+            this.upperRPMLimit = upperRPMLimit;
             this.rest = rest;
             this.notes = notes;
         }
